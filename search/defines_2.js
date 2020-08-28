e081ade5bf6996722a46b9def41b49f655e7a091
Travis CI <travis@travis-ci.org>
2020-08-28 13:06:42 +0000

    Deploy code docs to GitHub Pages Travis build: 21
    Commit: fe0fc7027cbb286ef0520f8132860887ccc9a21f

--- search/defines_2.js
+++ search/defines_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['is_5fcontinues_5fmode',['IS_CONTINUES_MODE',['../_erriez_b_h1750__regs_8h.html#a7de24f4b3e3f25cef4fcb791afac6a79',1,'ErriezBH1750_regs.h']]],
-  ['is_5finitialized',['IS_INITIALIZED',['../_erriez_b_h1750__regs_8h.html#a504622300699e7198fec1ba32a7dd58a',1,'ErriezBH1750_regs.h']]],
+  ['is_5finitialized',['IS_INITIALIZED',['../_erriez_b_h1750__regs_8h.html#ac6327472eb27d4cb28fd40089bbee436',1,'ErriezBH1750_regs.h']]],
   ['is_5flow_5fresolution',['IS_LOW_RESOLUTION',['../_erriez_b_h1750__regs_8h.html#aff331193cfe00a10aa0aff6261a50ec3',1,'ErriezBH1750_regs.h']]],
   ['is_5fone_5ftime_5fmode',['IS_ONE_TIME_MODE',['../_erriez_b_h1750__regs_8h.html#aaeb100631da432bd84c755efcab11cf6',1,'ErriezBH1750_regs.h']]]
 ];
